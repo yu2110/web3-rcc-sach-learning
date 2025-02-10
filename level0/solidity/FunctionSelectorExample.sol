f267cac264e42f5efabea083779793b2a48f96b0
yu2110 <yu.2110@163.com>
2025-02-10 18:00:29 +0800

    add FunctionSelectorExample

---
+++ level0/solidity/FunctionSelectorExample.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.20;
+
+contract FunctionSelectorExample{
+
+   function square(uint x) public  pure returns (uint){
+     return x * x;
+   }
+
+   function double(uint x) public  pure returns (uint){
+     return 2 * x;
+   }
+
+
+   //返回函数选择器
+   function getSelectorSquare() external  pure  returns(bytes4){
+     return this.square.selector;
+   }
+
+   function getSelectorDouble() external  pure  returns(bytes4){
+    return this.double.selector;
+
+   }
+
+   function getSelectorDouble1() external  pure  returns(bytes4){
+     return bytes4(keccak256("double(uint256)"));
+
+   }
+
+   //函数： 根据传入的选择器动态调用 square 或者double函数
+    function select(bytes4 selector, uint x) external returns (uint z) {
+        (bool success, bytes memory data) = address(this).call(abi.encodeWithSelector(selector, x));
+        require(success, "Function call failed");
+        z = abi.decode(data, (uint));
+    }
+
+
+}
