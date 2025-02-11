5a27549c05de51b8830f440fa3d990bab5087278
yu2110 <yu.2110@163.com>
2025-02-11 22:09:42 +0800

    TryCatch

---
+++ level0/solidity/TryCatchExample.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.20;
+
+
+contract TryCatchExample{
+   
+     function tryCatchDemo(address _contractAddress) public {
+           // 尝试调用外部合约的函数
+            try ExternalContract(_contractAddress).someFunction() {
+            // 处理成功
+            } catch {
+            // 处理失败
+            }
+     }
+
+}
+
+contract ExternalContract{
+    function someFunction() public returns(bool){
+        //可能抛出异常的函数
+    }
+}
