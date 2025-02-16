c6f1db1c79534ca85267f3f1e4bf857eb4f96b6c
sach <sach@sachdeMacBook-Pro.local>
2025-02-16 09:42:42 +0800

    git add

--- level0/solidity_contract/IERC777Recipient.sol
+++ level0/solidity_contract/IERC777Recipient.sol
@@ -1,7 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20; 
 //创建一个自定义 ERC777 代币，模拟一个功德箱合约，记录每位施主的捐赠。
-contract Merit{
+// import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/IERC777Recipient.sol";
+// import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC777/IERC777.sol";
+// import "@openzeppelin/contracts-ethereum-package/contracts/introspection/IERC1820Registry.sol";
+// contract Merit is IERC777Recipient {
+//     mapping(address => uint) public givers;
+//     address _owner;
+//     IERC777 _token;
+//     IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 
-
-}
+//     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = 0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
+//     constructor(IERC777 token) public {
+//         _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
+//         _owner = msg.sender;
+//         _token = token;
+//     }
+//     function tokensReceived(
+//         address operator,
+//         address from,
+//         address to,
+//         uint amount,
+//         bytes calldata userData,
+//         bytes calldata operatorData
+//     ) external {
+//         givers[from] += amount;
+//     }
+//     // 功德箱管理员提取代币
+//     function withdraw() external {
+//         require(msg.sender == _owner, "no permission");
+//         uint balance = _token.balanceOf(address(this));
+//         _token.send(_owner, balance, "");
+//     }
+// }
